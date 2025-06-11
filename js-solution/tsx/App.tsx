5330ef28324618775000b991637eeee2a8ffdaf6
Aaron Kim <ajkim19@gmail.com>
2025-06-11 02:50:36 +0000

    Created App.tsx

---
+++ js-solution/tsx/App.tsx
@@ -0,0 +1,12 @@
+import { NavBar } from './NavBar';
+import { NewEntry } from './NewEntry';
+import './App.css';
+
+export default function App() {
+  return (
+    <>
+      <NavBar />
+      <NewEntry />
+    </>
+  );
+}
