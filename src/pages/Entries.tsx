ecd66070485d7c573851954179852e0b4fc59338
Aaron Kim <ajkim19@gmail.com>
2025-06-23 22:20:06 +0000

    Created Entries page

---
+++ src/pages/Entries.tsx
@@ -0,0 +1,26 @@
+export function Entries() {
+  return (
+    <main className="container">
+      <div data-view="entries" className="entries-wrapper hidden">
+        <div className="entries-heading column-full">
+          <h1>Entries</h1>
+          <button
+            type="button"
+            className="new-entry-button"
+            data-view="entry-form">
+            New
+          </button>
+        </div>
+        <p className="no-entries-text">No entries have been recorded</p>
+        <ul className="entry-list"></ul>
+      </div>
+      {/* <dialog>
+        <p><strong>Are you sure you want to delete this entry?</strong></p>
+        <div className="modal-actions">
+          <button className="cancel-modal">CANCEL</button>
+          <button className="confirm-modal">CONFIRM</button>
+        </div>
+      </dialog> */}
+    </main>
+  );
+}
