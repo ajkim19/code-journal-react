0b9e1883492b27d44d1fa1ccec0bcf87dcd9cfcc
Aaron Kim <ajkim19@gmail.com>
2025-06-24 00:33:24 +0000

    Created EditEntry page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import { NavBar } from './components/NavBar';
 import { Entries } from './pages/Entries';
 import { NewEntry } from './pages/NewEntry';
+import { EditEntry } from './pages/EditEntry';
 
 export function App() {
   return (
@@ -9,6 +10,7 @@ export function App() {
       <Route path="/" element={<NavBar />}>
         <Route index element={<Entries />} />
         <Route path="/new-entry" element={<NewEntry />} />
+        <Route path="/edit-entry" element={<EditEntry />} />
       </Route>
     </Routes>
   );
---
+++ src/pages/EditEntry.tsx
@@ -0,0 +1,38 @@
+export function EditEntry() {
+  return (
+    <main className="container">
+      <div data-view="entry-form" className="entry-form-wrapper hidden">
+        <form id="entry-form">
+          <div className="column-full">
+            <h1 className="new-entry-header">New Entry</h1>
+          </div>
+          <div className="row">
+            <div className="photo-wrapper column-half">
+              <img
+                id="entry-image"
+                src="images/placeholder-image-square.jpg"
+                alt="Placeholder image"
+              />
+            </div>
+            <div className="column-half">
+              <label for="title">Title</label>
+              <input id="title" type="text" name="title" required />
+              <label for="photo-url">Photo URL</label>
+              <input id="photo-url" type="url" name="photoUrl" required />
+            </div>
+            <div className="column-full">
+              <label for="notes">Notes</label>
+              <textarea name="notes" id="notes" required></textarea>
+              <div className="form-actions">
+                <button className="delete-button hide" type="button">
+                  Delete Entry
+                </button>
+                <button type="submit">Save</button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
+}
